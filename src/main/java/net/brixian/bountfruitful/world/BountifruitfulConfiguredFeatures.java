ea684c276ccab186a61761dde1058cfec6139dc9
bluth <blutheguywhodraws@gmail.com>
2025-11-10 18:47:43 -0800

    Commit Message

--- src/main/java/net/brixian/bountfruitful/world/BountifruitfulConfiguredFeatures.java
+++ src/main/java/net/brixian/bountfruitful/world/BountifruitfulConfiguredFeatures.java
@@ -5,38 +5,41 @@ import net.minecraft.block.Blocks;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.structure.rule.BlockMatchRuleTest;
+import net.minecraft.structure.rule.RuleTest;
+import net.minecraft.structure.rule.TagMatchRuleTest;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.FeatureConfig;
-import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 
 public class BountifruitfulConfiguredFeatures {
-    public static final RegistryKey<ConfiguredFeature<?, ?>> MANGOSTEEN_KEY = registerKey("mangosteen_tree");
+
+	public static final RegistryKey<ConfiguredFeature<?, ?>> MANGOSTEEN_KEY = registerKey("driftwood");
+
+	public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> context) {
 
 
-    public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> context) {
-        register(context, MANGOSTEEN_KEY, Feature.TREE, new TreeFeatureConfig.Builder(
-                BlockStateProvider.of(Blocks.NETHERITE_BLOCK),
-                new StraightTrunkPlacer(5, 6, 3),
+		register(context, MANGOSTEEN_KEY, Feature.TREE, new TreeFeatureConfig.Builder(
+				BlockStateProvider.of(Blocks.NETHERITE_BLOCK),
+				new StraightTrunkPlacer(5, 6, 3),
 
-                BlockStateProvider.of(Blocks.MOSS_BLOCK),
-                new BlobFoliagePlacer(ConstantIntProvider.create(4), ConstantIntProvider.create(1), 3),
+				BlockStateProvider.of(Blocks.MOSS_BLOCK),
+				new BlobFoliagePlacer(ConstantIntProvider.create(4), ConstantIntProvider.create(1), 3),
 
-                new TwoLayersFeatureSize(1, 0, 2)).build());
+				new TwoLayersFeatureSize(1, 0, 2)).build());
+	}
 
-    }
+	public static RegistryKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+		return RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, Identifier.of(Bountifruitful.MOD_ID, name));
+	}
 
-    public static RegistryKey<ConfiguredFeature<?, ?>> registerKey(String name) {
-        return RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, Identifier.of(Bountifruitful.MOD_ID));
-    }
-    private static <FC extends FeatureConfig, F extends Feature<FC>> void register(Registerable<ConfiguredFeature<?, ?>> context,
-                                                                                   RegistryKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
-        context.register(key, new ConfiguredFeature<>(feature, configuration));
-    }
+	private static <FC extends FeatureConfig, F extends Feature<FC>> void register(Registerable<ConfiguredFeature<?, ?>> context,
+																				   RegistryKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
+		context.register(key, new ConfiguredFeature<>(feature, configuration));
+	}
 }
